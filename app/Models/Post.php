c290a04c39e3935ac18194e8d7c7264edec01c3f
raman <ramanmhedin@gmail.com>
2025-05-05 10:05:25 +0300

    Add Post model with relationships and fillable properties
    Introduce a `Post` model to manage posts within the application. Includes fillable fields and defines relationships to `Category` and `User` models.

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    protected $fillable=[
+        "title",
+        "content",
+        "author_id",
+        "category_id",
+        ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'author_id');
+    }
+}
