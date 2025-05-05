a2d93ee91694fe9da35972ea81cd3816753ea6fb
raman <ramanmhedin@gmail.com>
2025-05-05 15:21:01 +0300

    Add L5 Swagger for API documentation
    Integrated L5 Swagger package for generating API documentation, with configuration and views. Includes setup for login, registration, and logout endpoints using OpenAPI 3.0 specifications.

---
+++ app/Swagger/OpenApiSpec.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Blog API Development Task",
+ *     description="API documentation for Blog API",
+ *     @OA\Contact(
+ *         email="ramanmhedin@gmail.com"
+ *     )
+ * )
+ */
+class OpenApiSpec {}
