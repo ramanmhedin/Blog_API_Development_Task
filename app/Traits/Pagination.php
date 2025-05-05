9a391e46df252c7341e2537c3b8fbe6a759d8deb
raman <ramanmhedin@gmail.com>
2025-05-05 20:15:44 +0300

    Use array access for request parameters in Pagination trait
    Replaced the use of `get` method with array access syntax for retrieving request parameters. This simplifies code readability and ensures consistency across the trait.

--- app/Traits/Pagination.php
+++ app/Traits/Pagination.php
@@ -7,10 +7,10 @@ trait Pagination
 {
     public function orderAndPagination( $request, Builder $query)
     {
-        $orderBy = $request->get('order_by') ?? $query->getModel()->getKeyName();
-        $sort = $request->get('sort', 'desc');
-        $perPage = $request->get('per_page', 15);
-        $page = $request->get('page')??1;
+        $orderBy = $request['order_by'] ?? $query->getModel()->getKeyName();
+        $sort = $request['sort']??'desc';
+        $perPage = $request['per_page']?? 15;
+        $page = $request['page']??1;
 
         // Apply order and pagination
         return $query->orderBy($orderBy, $sort)->simplePaginate(perPage:$perPage,page:$page);
