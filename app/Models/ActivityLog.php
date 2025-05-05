75b8b7e9114594800dcb17ab849b242a48b2921b
raman <ramanmhedin@gmail.com>
2025-05-05 10:04:02 +0300

    Add ActivityLog model to track user activities
    Introduce the `ActivityLog` model with fillable properties to log actions performed on entities.

---
+++ app/Models/ActivityLog.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActivityLog extends Model
+{
+    //
+    protected $fillable = [
+        "performer",
+        "type_of_action",
+        "entity_type",
+        "entity_id",
+        "changed_fields",
+        "timestamp",
+    ];
+    public $timestamps = false;
+
+    protected static function booted(): void
+    {
+        static::creating(function ($task) {
+            $task->timestamp = now();
+        });
+    }
+
+}
